78d0207b89bace261dc92a622aa71a44217a4b25
JJ Asghar <jj@chef.io>
2018-09-15 16:33:56 -0500

    gcp

---
+++ gcp/export_me.sh
@@ -0,0 +1,9 @@
+#gcloud info
+
+#gcloud config list project
+
+export GCP_PROJECT_NAME=jjasghar
+export GCP_PROJECT_NUMBER=183117
+export GCP_PROJECT_ID=jjasghar-183117
+export GCP_LOCATION=us-central1
+export GCP_ZONE=us-central1-b
